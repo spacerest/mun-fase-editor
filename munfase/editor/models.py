2429063a801a1894d4f8001ad9cc67d4acc1d4f9
spacerest <robconvo@gmail.com>
2018-11-14 13:57:08 +0100

    fixed resize ratio (again) xD

--- munfase/editor/models.py
+++ munfase/editor/models.py
@@ -65,11 +65,11 @@ class UserUploadedImage(models.Model):
             new_width = image_size[0]
             new_height = image_size[1]
             if (image_width < image_height):
-                new_height = image_size[0]
-                new_width = int(image_size[1] * image_height / image_width)
+                new_width = image_size[0]
+                new_height = int(image_size[1] * image_height / image_width)
             elif (image_height < image_width):
-                new_width = image_size[1]
-                new_height = int(image_size[0] * image_height / image_width)
+                new_height = image_size[1]
+                new_width = int(image_size[0] * image_height / image_width)
             image = image.resize((new_width, new_height), Image.ANTIALIAS)
             image.save(image_filename)
 
